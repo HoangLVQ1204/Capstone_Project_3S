108b6f0e82328e6a836429c0ae6e1e4d45bfe441
quyennvse02884 <quyennv.bg@gmail.com>
2015-10-10 18:49:31 +0700

    [design] update list order

--- Develop/Sources/client-mobile/3S Shipper/www/js/controller/TasksController.js
+++ Develop/Sources/client-mobile/3S Shipper/www/js/controller/TasksController.js
@@ -3,15 +3,61 @@
  */
 app.controller('TasksCtrl', function ($scope) {
 
-  $scope.orderTasks = [
-    {orderID: 1, shipperID: 002, title: 'Angel cake' },
-    {orderID: 2, shipperID: 002, title: 'Angel food cake' },
-    {orderID: 3, shipperID: 002, title: 'Apple cake' },
-    {orderID: 4, shipperID: 002, title: 'Angel food cake' },
-    {orderID: 5, shipperID: 003, title: 'Applesauce cake' },
-    {orderID: 6, shipperID: 003, title: 'Aranygaluska' },
-    {orderID: 7, shipperID: 003, title: 'Babka' },
-    {orderID: 8, shipperID: 003, title: 'Better than sex cake' },
-    {orderID: 9, shipperID: 003, title: 'Banana cake/bread' }
-  ];
+  $scope.orders = [{
+    id: 1,
+    code: 'ORD001-001',
+    statusid: 'Done',
+    ordertypeid: '1',
+    pickupaddress: 'Đại Học Công Nghiệp - Từ Liêm - Hà Nội',
+    deliveryaddress: 'Đại Học Cảnh Sát - Cổ Nhuế - Hà Nội'
+  }, {
+    id: 2,
+    code: 'ORD002-202',
+    statusid: 'Picking',
+    ordertypeid: '2',
+    pickupaddress: 'Từ Liêm - Hà Nôi',
+    deliveryaddress: 'Quận Đống Đa - Hà Nội'
+  }, {
+    id: 3,
+    code: 'ORD888-222',
+    statusid: 'Delivering',
+    ordertypeid: '2',
+    pickupaddress: 'Phường Nguyễn Du - Quận Hai Bà Trưng - Hà Nội',
+    deliveryaddress: 'Phường Quỳnh Mai - Quận Hai Bà Trưng - Hà Nội'
+  }, {
+    id: 4,
+    code: 'ORD999-000',
+    statusid: 'Pending',
+    ordertypeid: '1',
+    pickupaddress: 'Phường Bồ Đề - Long Biên - Hà Nội',
+    deliveryaddress: 'Mễ Trì - Nam Từ Liêm - Hà Nội'
+  }, {
+    id: 5,
+    code: 'ORD222-123',
+    statusid: 'Picking',
+    ordertypeid: '2',
+    pickupaddress: 'Xuân Phương - NamTừ Liêm - Hà Nôi',
+    deliveryaddress: 'Tràng Tiền - Hoàn Kiếm - Hà Nội'
+  }, {
+    id: 6,
+    code: 'ORD002-90933',
+    statusid: 'Pending',
+    ordertypeid: '1',
+    pickupaddress: 'Quang Trung - Hà Đông - Hà Nôi',
+    deliveryaddress: 'Hàng Bông - Quận Hoàn Kiếm - Hà Nội'
+  }, {
+    id: 7,
+    code: 'ORD022-252',
+    statusid: 'Picking',
+    ordertypeid: '2',
+    pickupaddress: 'Từ Liêm - Hà Nôi',
+    deliveryaddress: 'Quận Đống Đa - Hà Nội'
+  }, {
+    id: 8,
+    code: 'ORD032302-222',
+    statusid: 'Delivering',
+    ordertypeid: '2',
+    pickupaddress: 'Xuân Phương - Nam Từ Liêm - Hà Nôi',
+    deliveryaddress: 'Quận Đống Đa - Hà Nội'
+  }];
 });
