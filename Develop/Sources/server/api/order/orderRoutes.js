aec02c0a7d1f5085c6835f25e29ebc9d06c46b53
Cao Khanh <kck.vnclub@gmail.com>
2015-10-16 08:01:51 +0700

    change

--- Develop/Sources/server/api/order/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Cao Khanh on 15/10/2015.
- */
-module.exports = function(app){
-    var controller = require('./orderController')(app);
-
-    app.route('/api/orders')
-        .get(controller.get)
-}
