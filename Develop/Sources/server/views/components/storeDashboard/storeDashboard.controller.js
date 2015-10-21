dd7f14955aa17935520e9e7c3cc8a1cd3648f2e4
Cao Khanh <kck.vnclub@gmail.com>
2015-10-21 19:06:12 +0700

    Sửa dashboard

--- Develop/Sources/server/views/components/storeDashboard/storeDashboard.controller.js
+++ Develop/Sources/server/views/components/storeDashboard/storeDashboard.controller.js
@@ -2,27 +2,27 @@
  * Created by hoanglvq on 9/22/15.
  */
 
-function storeDashboardController($scope,$state){
+function storeDashboardController($scope,$state,dataService){
 
 
     $scope.$watch('$viewContentLoaded', function(event) {
         caplet();
     });
 
-    //getDataFromServer();
-    //
-    //function getDataFromServer() {
-    //    var urlBase = 'http://localhost:3000/orders';
-    //    dataService.getDataServer(urlBase)
-    //        .success(function (rs) {
-    //            $scope.ordersWaiting = rs['Waiting'];
-    //            $scope.orderCarring = rs['Carrying'];
-    //            console.log(rs);
-    //        })
-    //        .error(function (error) {
-    //            console.log('Unable to load customer data: ' + error);
-    //        });
-    //}
+    getDataFromServer();
+
+    function getDataFromServer() {
+        var urlBase = 'http://localhost:3000/orders';
+        dataService.getDataServer(urlBase)
+            .success(function (rs) {
+                $scope.ordersWaiting = rs['Waiting'];
+                $scope.orderCarring = rs['Carrying'];
+                console.log(rs);
+            })
+            .error(function (error) {
+                console.log('Unable to load customer data: ' + error);
+            });
+    }
 }
 
 
