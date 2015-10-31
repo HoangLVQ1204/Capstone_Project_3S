18b94017862e9a71c9beef668a8267d2fdab5e4b
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2015-11-01 00:47:27 +0700

    Shipper Change Status

---
+++ Develop/Sources/client-mobile/3S Shipper/www/js/controller/ChangeStatusController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Kaka Hoang Huy on 11/1/2015.
+ */
+
+app.controller('ChangeStatusCtrl', ['$scope', function($scope) {
+
+  //set Option Selected
+  $scope.changeStatus = function(){
+    console.log('change');
+  }
+}]);
