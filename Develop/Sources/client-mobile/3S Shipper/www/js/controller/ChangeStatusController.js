e0f28b9e1b1aa7ad1906e1fabf7ebc6e132d162f
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2015-11-01 20:30:13 +0700

    Mobile: Change Status

--- Develop/Sources/client-mobile/3S Shipper/www/js/controller/ChangeStatusController.js
+++ Develop/Sources/client-mobile/3S Shipper/www/js/controller/ChangeStatusController.js
@@ -5,7 +5,8 @@
 app.controller('ChangeStatusCtrl', ['$scope', function($scope) {
 
   //set Option Selected
-  $scope.changeStatus = function(){
-    console.log('change');
+  $scope.status = true;
+  $scope.changeStatus = function(status){
+    console.log('change',status);
   }
 }]);
