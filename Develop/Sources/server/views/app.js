5b61b8c7e27287e049b744831007f57ce0731c15
HoangLVQ <hoanglvqse90184@fpt.edu>
2015-11-05 00:08:35 +0700

    no message

--- Develop/Sources/server/views/app.js
+++ Develop/Sources/server/views/app.js
@@ -17,7 +17,7 @@ angular.module('app', [
         admin: 3
     },
 
-    baseURI: "http://localhost:3000"
+    baseURI: "ec2-52-32-233-25.us-west-2.compute.amazonaws.com"
 
 }).config(function($stateProvider,$urlRouterProvider,$httpProvider,jwtInterceptorProvider,uiGmapGoogleMapApiProvider,config){
 
