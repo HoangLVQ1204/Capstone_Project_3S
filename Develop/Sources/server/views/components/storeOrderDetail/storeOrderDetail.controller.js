1a8edfec433c1025d0adc20da11bf020830e90b4
KhanhKC <kck.vnclub@gmail.com>
2015-11-16 20:35:53 +0700

    storeorderdetail

--- Develop/Sources/server/views/components/storeOrderDetail/storeOrderDetail.controller.js
+++ Develop/Sources/server/views/components/storeOrderDetail/storeOrderDetail.controller.js
@@ -1,13 +1,28 @@
 /**
- * Created by hoanglvq on 9/22/15.
+ * Created by khanhkc on 14/11/15.
  */
 
-function storeOrderDetailController($scope,$state,dataService, $http, config){    
+function storeOrderDetailController($scope,$stateParams,dataService, $http, config){    
+	
+	$scope.orderid = $stateParams.orderid;
 
+	getDataFromServer();
+    function getDataFromServer() {
+        var urlBase = config.baseURI + '/orders/' + $scope.orderid;
+        dataService.getDataServer(urlBase)
+            .success(function (rs) {               
+               $scope.data = rs;
+               $scope.listgoods = rs.goods;
+               console.log("==============rs===========",rs);
+            })
+            .error(function (error) {
+                console.log('Unable to load order: ' + error);
+            });
+    }
 }
 
 
-storeOrderDetailController.$inject = ['$scope','$state','dataService','$http','config'];
+storeOrderDetailController.$inject = ['$scope','$stateParams','dataService','$http','config'];
 
 angular.module('app').controller('storeOrderDetailController',storeOrderDetailController);
 
