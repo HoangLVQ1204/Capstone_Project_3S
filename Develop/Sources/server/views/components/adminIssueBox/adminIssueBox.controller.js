c9d6c424d5e3177151a3e4361911c6c790d5a1f9
HoangNK93 <hoangnkse02564@fpt.edu.vn>
2015-11-17 23:56:50 +0700

    [Update] IssueBox socket

--- Develop/Sources/server/views/components/adminIssueBox/adminIssueBox.controller.js
+++ Develop/Sources/server/views/components/adminIssueBox/adminIssueBox.controller.js
@@ -2,22 +2,27 @@
  * Created by Hoang on 10/18/2015.
  */
 
-function adminIssueBoxController($scope,$state, $http, $filter, config) {
+function adminIssueBoxController($scope,$state, $http, $filter, config, $rootScope) {
     //alert(0);
-    $scope.issueList = [];
-    $http.get(config.baseURI + "/api/getAllIssue").success(function(response){
-        $scope.issueList = response;
-        $scope.issueList.sort( $scope.sortByDate);
-        //$scope.displayedOrderCollection = [].concat($scope.orderList);
-        //console.log($scope.issueList)
-    }).then(function () {
+    getDataFromSever();
 
-        $state.go('admin.issueBox.content',{issueid: $scope.issueList[0].issueid},{
-            //reload: true,
-        });
+    function getDataFromSever() {
+        $scope.issueList = [];
+        $http.get(config.baseURI + "/api/getAllIssue").success(function(response){
+            $scope.issueList = response;
+            $scope.issueList.sort( $scope.sortByDate);
+            //$scope.displayedOrderCollection = [].concat($scope.orderList);
+            //console.log($scope.issueList)
+        }).then(function () {
 
-        //alert(1);
-    })
+            $state.go('admin.issueBox.content',{issueid: $scope.issueList[0].issueid},{
+                //reload: true,
+            });
+
+            //alert(1);
+        })
+    }
+
 
     $scope.sortByDate = function (x, y) {
             if (x.createddate > y.createddate) {
@@ -49,7 +54,16 @@ function adminIssueBoxController($scope,$state, $http, $filter, config) {
     });
 
 
+    // START Listen to socket changes
+    $rootScope.$on("admin:issue:newIssue", function(event, args){
+        //alert(args.message);
+        getDataFromSever();
+        //$scope.$apply();
+        //getDataFromServer();
+        // console.log( $scope.onlineShipper);
+        //$scope.onlineShipper = 10;
+    });
 }
 
-adminIssueBoxController.$inject = ['$scope','$state', '$http', '$filter','config'];
+adminIssueBoxController.$inject = ['$scope','$state', '$http', '$filter','config','$rootScope'];
 angular.module('app').controller('adminIssueBoxController',adminIssueBoxController);
