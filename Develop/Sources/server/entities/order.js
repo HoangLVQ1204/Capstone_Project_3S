e8e91e0507548bd8069eb897a72309b0dd5d6c85
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2015-11-24 03:14:04 +0700

    [Fix bug] check undefined of ward, district, province

--- Develop/Sources/server/entities/order.js
+++ Develop/Sources/server/entities/order.js
@@ -103,7 +103,11 @@ module.exports = function(sequelize, DataTypes) {
       },
       getCustomerAddress: function(){
         var addressList = require("../config/address.json")
-        return this.deliveryaddress + ', ' + addressList.ward[this.deliverywardid] + ', ' + addressList.district[this.deliverydistrictid] + ', ' + addressList.province[this.deliveryprovinceid]
+        var address = (this.deliveryaddress) ? this.deliveryaddress : '';
+        var wardText = (this.deliverywardid) ? addressList.ward[this.deliverywardid] : '';
+        var districtText = (this.deliverydistrictid) ? addressList.district[this.deliverydistrictid] : '';
+        var provinceText = (this.deliveryprovinceid) ? addressList.province[this.deliveryprovinceid] : '';
+        return address + ', ' + wardText + ', ' + districtText + ', ' + provinceText;
       }
     },
     classMethods: {
