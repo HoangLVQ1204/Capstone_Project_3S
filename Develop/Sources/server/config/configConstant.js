512b988f992e905aecbfcc2cfd73953503f77c51
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2015-11-26 00:07:42 +0700

    [Fix bug] Display Stock information for normal order

--- Develop/Sources/server/config/configConstant.js
+++ Develop/Sources/server/config/configConstant.js
@@ -9,6 +9,11 @@ var constant = {
         EXPRESS_TYPE: 1,
         COMMON_TYPE: 2
     },
-    statusList: pathToStatusList
+    statusList: pathToStatusList,
+    stockID: 1,
+    doneStatus: 7,
+    inStockStatus: 4,
+    taskActive: 2,
+    taskDone: 3
 };
 module.exports = constant;
