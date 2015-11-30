b2790d7d5ebb5cd98f6e3f621f4686962d8f6866
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2015-11-30 10:22:17 +0700

    UT for API get

--- Develop/Sources/server/test/server.js
+++ Develop/Sources/server/test/server.js
@@ -14,27 +14,42 @@ var request = require("request");
 describe("Shipper APIs", function() {
 
     describe("Get list of tasks", function() {
+        var url = "http://localhost:3000/api/tasks";
         var options = {
-            url: "http://localhost:3000/api/tasks",
+            url: url,
             headers: {
                 Authorization:'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlNQMDAwMDAxIiwidXNlcnJvbGUiOjEsInVzZXJzdGF0dXMiOjIsInRpbWUiOiIyMDE1LTExLTI5VDE5OjQ5OjUxLjczMFoiLCJpYXQiOjE0NDg4MjY1OTEsImV4cCI6MTQ0OTY5MDU5MX0.3Z995aY2BGgIbkTWOxvPoVDZTUXoaQ96QrI9ItKjWvU'
             }
         };
 
-        it("returns status 200", function(done) {
+        it("returns ok", function(done) {
             request(options, function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
+                expect(body).not.to.be.null;
                 done();
             });
         });
 
-        it("returns detail not null", function(done) {
-            request(options, function(error, response, body) {
+        it("returns fail without token", function(done) {
+            request(url, function(error, response, body) {
                 //expect(body).to.equal("");
+                expect(response.statusCode).to.equal(401);
                 expect(body).not.to.be.null;
                 done();
             });
         });
 
     });
+
+    //it("should display input text from a form.", function(done){
+    //    request.post('localhost:8080')
+    //        .type('form')
+    //        .send({field: "Test string."})
+    //        .end(function(res){
+    //            expect(res).to.exist;
+    //            expect(res.status).to.equal(200);
+    //            expect(res.text).to.contain("Test");
+    //            done();
+    //        })
+    //});
 });
