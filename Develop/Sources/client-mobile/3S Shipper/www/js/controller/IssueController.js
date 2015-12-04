9bfa7d3da22607a5b8d432aa33f1fa61ae64d6f5
quyennvse02884 <quyennv.bg@gmail.com>
2015-12-04 20:21:16 +0700

    edit msg Isue Screen

--- Develop/Sources/client-mobile/3S Shipper/www/js/controller/IssueController.js
+++ Develop/Sources/client-mobile/3S Shipper/www/js/controller/IssueController.js
@@ -271,25 +271,25 @@ app.controller('IssueCtrl',['$scope','$ionicPopup', 'dataService', '$ionicLoadin
       if ( typeof issue === "undefined" || issue.category === null || typeof issue.category === "undefined") {
         var msg1 = {
           id: 999,
-          content: 'Please choose Type !'
+          content: 'Please choose Category'
         }
         $scope.showAlert(msg1);
       } else if (issue.type === null || typeof issue.type === "undefined") {
         var msg2 = {
           id: 999,
-          content: 'Please choose Reason !'
+          content: 'Please choose Reason'
         }
         $scope.showAlert(msg2);
       } else if (issue.category.categoryID != 1 && typeof issue.issuedOrder === "undefined") {
         var msg3 = {
           id: 999,
-          content: 'Please choose Order !'
+          content: 'Please choose Order'
         }
         $scope.showAlert(msg3);
       } else if (typeof issue.description === "undefined" || issue.description === "") {
         var msg4 = {
           id: 999,
-          content: 'Please write Content !'
+          content: 'Please write Content'
         }
         $scope.showAlert(msg4);
       } else {
@@ -349,7 +349,7 @@ app.controller('IssueCtrl',['$scope','$ionicPopup', 'dataService', '$ionicLoadin
               rs = rs.data
               $ionicPopup.alert({
                 title: 'Success',
-                content: 'Your Issue is sent to Admin'
+                content: 'Your Issue is sent to Admin. Wait for Admin to solve.'
               }).then(function(res) {
                 $scope.isSend = false;
 
