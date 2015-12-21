b8034e67ec20f9bf88e324ea70c6471a1fca2173
HoangLVQ <hoanglvqse90184@fpt.edu>
2015-12-22 01:49:01 +0700

    Frond-end update

--- Develop/Sources/server/views/frontEnd/js/main.js
+++ Develop/Sources/server/views/frontEnd/js/main.js
@@ -370,7 +370,7 @@ function init() {
 
     // Basic options for a simple Google Map
     // The latitude and longitude to center the map (always required)
-    var center = new google.maps.LatLng(40.70820, -74.00487);
+    var center = new google.maps.LatLng(21.013663, 105.527500);
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
@@ -540,7 +540,7 @@ function init() {
     var map = new google.maps.Map(document.getElementById('map'), mapOptions, center);
 
     var locations = [
-        ['<h6>Bondi Beach, East Coast</h6><p>This is where we are currently, the sun is up...<br><i class="fa fa-coffee"></i> Visit us, we pay the coffee!</p>', 40.70820, -74.00487, 4],
+        ['<h6>KarryWell Corp</h6><p>Welcome to us</p>', 21.013663, 105.527500, 4],
         // ['<h6>Coogee Beach, East Coast</h6><p>Opening Hours<br><i class="fa fa-clock-o"></i> 8:00 to 22:00</p>', 40.66810, -73.94480, 5],
         // ['<h6>Cronulla Beach, East Coast</h6><p>Opening Hours<br><i class="fa fa-clock-o"></i> 10:00 to 20:00</p>', 40.80427, -74.01208, 3],
         // ['<h6>Manly Beach, East Coast</h6><p>Opening Hours<br><i class="fa fa-clock-o"></i> 6:00 to 14:00</p>', 40.76759, -73.96756, 2],
@@ -550,6 +550,11 @@ function init() {
     var infowindow = new google.maps.InfoWindow();
 
     var marker, i;
+
+    //marker = new google.maps.Marker({
+    //            position: new google.maps.LatLng(21.013663, 105.527500),
+    //            map: map
+    //        });
 
     for (i = 0; i < locations.length; i++) {
         marker = new google.maps.Marker({
